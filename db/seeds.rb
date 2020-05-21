4cec406d39bbe9261299eae52265ec51c816f87c
matthewsedlacek <sedlacek1991@gmail.com>
2020-05-21 14:27:30 -0700

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+Song.destroy_all
+
+Song.create(title:"Every now and Then", released: false, release_year: 1991, artist_name: "Matthew", genre: "Rap")
+Song.create(title:"Everytime", released: true, release_year: 2006, artist_name: "Cascadia", genre: "EDM")
+Song.create(title:"Then", released: false, release_year: 1999, artist_name: "Sean", genre: "Rap")
+Song.create(title:"now and Then", released: false, release_year: 2010, artist_name: "Edward", genre: "Country")
+Song.create(title:"On The Mower", released: false, release_year: 2019, artist_name: "Michael", genre: "Jazz")
